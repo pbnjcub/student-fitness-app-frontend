08f43acc7ba8ff42ff4c97dfa433c67f7bb16d00
Paul Jung <pbnjcub@gmail.com>
2024-09-30 12:58:27 -0400

    updating to latest react-native

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,22 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['module:metro-react-native-babel-preset'],
-    plugins: ['react-native-reanimated/plugin'],
+    presets: [
+      'module:metro-react-native-babel-preset'
+    ],
+    plugins: [
+      'react-native-reanimated/plugin',
+      '@babel/plugin-transform-private-methods',
+      '@babel/plugin-transform-numeric-separator',
+      '@babel/plugin-transform-class-properties',
+      '@babel/plugin-transform-async-generator-functions',
+      '@babel/plugin-transform-export-namespace-from',
+      '@babel/plugin-transform-json-strings',
+      '@babel/plugin-transform-unicode-property-regex',
+      '@babel/plugin-transform-logical-assignment-operators',
+      '@babel/plugin-transform-dynamic-import'
+    ],
   };
 };
+
 
